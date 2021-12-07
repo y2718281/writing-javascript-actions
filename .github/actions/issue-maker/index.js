dc5cfc649495fafe18a988ad83340786a644ebe9
operation_jhorvat3 <jhorvat3@p-management01-vm.yk1blk0wzlkefjhsah0wmuge2g.ax.internal.cloudapp.net>
2021-12-07 09:45:28 +0000

    try to fix create method of octokit in index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,7 +9,7 @@ async function run() {
 
     const octokit = new github.getOctokit(token);
 
-    const newIssue = await octokit.issues.create.rest({
+    const newIssue = await octokit.rest.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
